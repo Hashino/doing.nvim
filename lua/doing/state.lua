d85e30556c043da202c52db5e8a1d80167893be0
Hashino <iam.ravara@gmail.com>
2025-04-14 15:13:02 -0300

    refactor(state.lua): simplified and commented code

--- lua/doing/state.lua
+++ lua/doing/state.lua
@@ -9,21 +9,16 @@ local State = {
 
 local tasks_file = ""
 
-local function load_tasks()
-  tasks_file = vim.fn.getcwd()
-     .. utils.os_path_separator()
-     .. config.options.store.file_name
-
-  local ok, res = pcall(vim.fn.readfile, tasks_file)
-  State.tasks = ok and res or {}
-end
-
-load_tasks()
-
 -- reloads tasks when directory changes and on startup
 vim.api.nvim_create_autocmd({ "DirChanged", "VimEnter", }, {
   callback = function()
-    load_tasks()
+    tasks_file = vim.fn.getcwd()
+       .. utils.os_path_separator()
+       .. config.options.store.file_name
+
+    local ok, res = pcall(vim.fn.readfile, tasks_file)
+    State.tasks = ok and res or {}
+
     State.task_modified()
   end,
 })
@@ -36,16 +31,17 @@ local function sync()
       local ok, err, err_name = (vim.uv or vim.loop).fs_unlink(tasks_file)
 
       if not ok then
-        utils.notify(tostring(err_name) .. ":" .. tostring(err), vim.log.levels.ERROR)
+        utils.notify("error deleting tasks file: " .. tostring(err_name) .. "\n" .. err,
+          vim.log.levels.ERROR)
       end
     end)()
-  end
-
-  if #State.tasks > 0 then
-    local ok, err = pcall(vim.fn.writefile, State.tasks, tasks_file)
+  elseif #State.tasks > 0 then
+    -- if tasks exist, write them to the file
+    local ok, err, err_name = pcall(vim.fn.writefile, State.tasks, tasks_file)
 
     if not ok then
-      utils.notify("error writing to tasks file:\n" .. err, vim.log.levels.ERROR)
+      utils.notify("error writing to tasks file:" .. tostring(err_name) .. "\n" .. err,
+        vim.log.levels.ERROR)
     end
   end
 end
@@ -58,23 +54,34 @@ end
 ---@return string current current plugin task or message
 function State.status(force)
   if (State.view_enabled or force) and utils.should_display() then
-    local count = #State.tasks or 0
     if State.message then
       return State.message
-    elseif count > 0 then
-      local tasks_left = ""
+    elseif #State.tasks > 0 then
+      local status = config.options.doing_prefix .. State.tasks[1]
 
       -- append task count number if there is more than 1 task
-      if config.options.show_remaining and count > 1 then
-        tasks_left = "  +" .. (count - 1) .. " more"
+      if config.options.show_remaining and #State.tasks > 1 then
+        status = status .. "  +" .. (#State.tasks - 1) .. " more"
       end
 
-      return config.options.doing_prefix .. State.tasks[1] .. tasks_left
+      return status
     elseif force then
       return "Not doing any tasks"
     end
   end
   return ""
+end
+
+function State.add(task, to_front)
+  if to_front then
+    table.insert(State.tasks, 1, task)
+  else
+    table.insert(State.tasks, task)
+  end
+end
+
+function State.done()
+  table.remove(State.tasks, 1)
 end
 
 ---show a message for the duration of `options.message_timeout` or timeout
@@ -98,18 +105,6 @@ end
 function State.task_modified()
   vim.api.nvim_exec_autocmds("User", { pattern = "TaskModified", })
   return config.options.store.sync_tasks and sync()
-end
-
-function State.add(task, to_front)
-  if to_front then
-    table.insert(State.tasks, 1, task)
-  else
-    table.insert(State.tasks, task)
-  end
-end
-
-function State.done()
-  table.remove(State.tasks, 1)
 end
 
 return State
